5f1a5c5365b48c89dfbfc7f2ae098a297180e64c
pepega <aristockij@gmail.com>
2025-02-04 18:24:09 +0300

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
+  base: "/signin_test",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
